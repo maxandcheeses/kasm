ee9672513ce0ae727a6ec1f60dde8b5cbdf1ae65
maxandcheeses <164303325+maxandcheeses@users.noreply.github.com>
2025-07-06 14:24:34 -0700

    Update install-kasm-manager.sh

--- kasm/install-kasm-manager.sh
+++ kasm/install-kasm-manager.sh
@@ -64,6 +64,6 @@ cat <<EOF
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/$KASM_TAR
 tar -xf $KASM_TAR
-sudo bash kasm_release/install.sh --role agent --public-hostname <AGENT_IP_ADDRESS> --manager-hostname <HOST_IP> --manager-token $MANAGER_TOKEN"
+sudo bash kasm_release/install.sh --role agent --public-hostname <AGENT_IP_ADDRESS> --manager-hostname <HOST_IP> --manager-token "$MANAGER_TOKEN"
 
 EOF
